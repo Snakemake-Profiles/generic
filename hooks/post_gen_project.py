d400a2bf7a4d3d0f75343e81dd9ab02fe286ae0c
kiesers <silas.kieser@gmail.com>
2018-12-06 16:21:06 +0100

    corected merge

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,11 +8,7 @@ for root, dirs, files in os.walk(TARGET):
        with open(os.path.join(root, filename)) as f:
            content = f.read()
        # replace tag by install path
-<<<<<<< HEAD
        content = content.replace('$((INSTALDIR))', TARGET)
-=======
-       content = content.replace('$((INSTALLDIR))', TARGET)
->>>>>>> 6af7df29a723d8a3ed6a4d9019923171a8053981
        # replace file content
        with open(os.path.join(root, filename), 'w') as f:
            f.write(content)
