eaf34c55fac0a8a04cff1dc5abc5194cf28cfa6c
Silas Kieser <SilasK@users.noreply.github.com>
2018-12-06 10:40:34 +0100

    check that returned job id is an int

--- {{cookiecutter.profile_name}}/scheduler.py
+++ {{cookiecutter.profile_name}}/scheduler.py
@@ -51,6 +51,8 @@ eprint("submit command: "+command)
 p = Popen(command.split(' '), stdout=PIPE, stderr=PIPE)
 output, error = p.communicate()
 if p.returncode != 0:
-   raise Exception("Job can't be submitted\n"+output.decode("utf-8")+error.decode("utf-8"))
+    raise Exception("Job can't be submitted\n"+output.decode("utf-8")+error.decode("utf-8"))
 else:
-	print(output.decode("utf-8"))
+    res= output.decode("utf-8")
+    jobid= int(res.strip().split()[-1])
+    print(jobid)
