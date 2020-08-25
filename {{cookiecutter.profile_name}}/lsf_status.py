63915e06698cb9ae7da78292d19fe6c921260b73
kiesers <silas.kieser@gmail.com>
2020-08-25 17:25:28 +0200

    adapt to lsf

--- {{cookiecutter.profile_name}}/lsf_status.py
+++ {{cookiecutter.profile_name}}/lsf_status.py
@@ -11,13 +11,13 @@ jobid = sys.argv[1]
 
 out= subprocess.run(['bjobs','-noheader',jobid],stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-state = out.sep()[2]
+state = out.strip().split()[2]
 
 
 map_state={"PEND":'running',
-           "RUN":'running', 
+           "RUN":'running',
            "PROV":"running",
-           "WAIT":'running', 
+           "WAIT":'running',
            "DONE":'success',
            "":'success'}
 
