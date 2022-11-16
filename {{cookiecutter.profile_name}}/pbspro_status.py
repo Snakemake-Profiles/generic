add790f66d45470f6118350c9a33e8c743479bae
Giulio Centorame <46871438+GiulioCentorame@users.noreply.github.com>
2022-11-16 15:07:14 +1000

    Rework job tracing conditions

--- {{cookiecutter.profile_name}}/pbspro_status.py
+++ {{cookiecutter.profile_name}}/pbspro_status.py
@@ -15,17 +15,25 @@ try:
         shell=True,
     )
 
+    tracejob = subprocess.run(
+        "tracejob -n 1 {}".format(jobid),
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True
+    )
+
     jsondoc = json.loads(res.stdout.decode())
     job_state = jsondoc["Jobs"][jobid]["job_state"]
 
-    if job_state == "C":
-        exit_status = jsondoc["Jobs"][jobid]["Exit_status"]
-        if exit_status == "0":
+    if job_state:
+        print("running")
+    else:
+        exit_status_0 = "Exit_status=0" in tracejob.stdout
+        if exit_status_0:
             print("success")
         else:
             print("failed")
-    else:
-        print("running")
 
 except (subprocess.CalledProcessError, IndexError, KeyboardInterrupt) as e:
     print("failed")
